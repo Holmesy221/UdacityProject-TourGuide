5a2582fdd1c6f2e9c4050999056f9379e85c8bf1
Holmesy221 <holmesy221@gmail.com>
2018-03-13 17:00:30 +0000

    ViewPager + Tablayout done

--- app/src/main/java/holmes/holmesy/tourguide/MainActivity.java
+++ app/src/main/java/holmes/holmesy/tourguide/MainActivity.java
@@ -1,7 +1,9 @@
 package holmes.holmesy.tourguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        PageAdapter pageAdapter = new PageAdapter(this, getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+
 }
